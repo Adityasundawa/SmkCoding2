6798b6c180fd3a78a84666e58cf83a4e09f4c1fc
Adityasundawa <adityasundawa.co@gmail.com>
2020-05-12 11:07:10 +0700

    ADD FRAGMENT

--- app/src/main/java/com/adityasundawa/lawancorona/MainActivity.kt
+++ app/src/main/java/com/adityasundawa/lawancorona/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.adityasundawa.lawancorona
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val menuText = arrayOf("Home","Covid","News","Profile")
+    val menuIcon = arrayOf(R.drawable.home,R.drawable.covid,R.drawable.news,R.drawable.profile)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ViewPagerAdapter(this)
+        view_pager.setAdapter(adapter);
+        TabLayoutMediator(tab_layout, view_pager,
+            TabConfigurationStrategy { tab, position ->
+                tab.text = menuText[position]
+                tab.icon = ResourcesCompat.getDrawable(resources,
+                    menuIcon[position], null)
+            }).attach()
     }
 }
---
+++ app/src/main/java/com/adityasundawa/lawancorona/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.adityasundawa.lawancorona
+
+import android.content.Context
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+FragmentStateAdapter(fragmentActivity) {
+    private val JUMLAH_MENU = 4
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> { return Home_Fragment() }
+            1 -> { return Covid_Fragment() }
+            2 -> { return News_Fragment() }
+            3 -> { return ProfileFragment()}
+            else -> {
+                return Covid_Fragment()
+            }
+        }
+    }
+    override fun getItemCount(): Int {
+        return JUMLAH_MENU
+    }
+}
