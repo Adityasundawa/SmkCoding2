09952f78bd323f813608a7fa4d55e989e2498bda
Adityasundawa <adityasundawa.co@gmail.com>
2020-05-17 04:30:50 +0700

    Tambah Splash Screen

--- app/src/main/java/com/adityasundawa/lawancorona/SplashScreen.kt
+++ app/src/main/java/com/adityasundawa/lawancorona/SplashScreen.kt
@@ -1,7 +1,9 @@
 package com.adityasundawa.lawancorona
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
@@ -9,5 +11,9 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },4000)
     }
 }
